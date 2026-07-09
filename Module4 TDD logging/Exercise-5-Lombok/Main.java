ffd860a1b8652e432f9d57e3cdf97fbc17994adb
GODUGULA GOUTHAM <godugulagoutham2006@gmail.com>
2026-07-09 22:54:26 +0530

    Add Main class to demonstrate Employee usage

---
+++ Module4 TDD logging/Exercise-5-Lombok/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee(101, "Rahul", 45000.0);
+        System.out.println(employee.getName());
+        System.out.println(employee);
+    }
+}
